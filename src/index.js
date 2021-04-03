3af5d0258e4dd321994f03b477dfc2e06b8f1baf
luiscostalafc <luiscostalafc@gmail.com>
2021-04-03 19:45:10 -0300

    feat: create update user

--- src/index.js
+++ src/index.js
@@ -81,7 +81,7 @@ app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
   customer.statement.push(statementOperation);
 
   return response.status(201).send();
-})
+});
 
 app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
   const { amount } = request.body;
@@ -103,6 +103,34 @@ app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
 
   return response.status(201).send();
 
-})
+});
+
+app.get("/statement/date", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  const { date } = request.query;
+
+  const dateFormat = new Date(date + " 00:00");
+
+  const statement = customer.statement.filter((statement) =>
+    statement.created_at.toDateString() === new Date(dateFormat).toDateString())
+
+
+  return response.json(statement);
+});
+
+app.put("/account", verifyIfExistsAccountCPF, (request, response) => {
+  const { name } = request.body;
+  const { customer } = request;
+
+  customer.name = name;
+
+  return response.status(201).send();
+});
+
+app.get("/account", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  return response.json(customer);
+});
 
 app.listen(3333);
